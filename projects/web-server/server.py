f370fcda206cc2ccf2405668428eef3c168aba18
james.taggart.43 <james.taggart.43@gmail.com>
2020-09-10 15:28:15 -0700

    Python - Flask server setup
    Notes on setting up flask. Virtual env setup. Install Flask.
    Import and use flask. Using render template to render html files.
    
    Python flask configure and render template.

---
+++ projects/web-server/server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/blog')
+def blog():
+    return 'These are my thoughts on blogs'
+
+@app.route('/blog/2020/dogs')
+def blog2():
+    return 'this is my dog'
